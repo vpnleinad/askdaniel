64c8ca9ee822c5c1b827df218c875f553b83d87c
Daniel Mello <danielmellofarias@gmail.com>
2022-05-12 13:42:52 -0300

    make it better

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='127.0.0.0', port='8080', debug=False)
